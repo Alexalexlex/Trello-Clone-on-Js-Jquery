4659392c24739d8d4de015ee2db1c27e1133bb43
leviossa <leviossa@Mac-mini-leviossa.local>
2020-09-08 18:12:01 +0300

    add render cols, need fix render task

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 $(document).ready(() => {
 
+$('.list-field').html(`<div class="box">
+<h5>My first board</h5>
+<ul class="canban list-group">
+</ul>
+<button type="button" class="mybtn list-group-item">Add new card</button>
+</div>`)
+
 const arrayTask = [];
+const cols = new Map ();
+
+// render cols
+const render = function() {
+  str = ``;
+  for(let headline of cols.keys()){
+    str+= `<div class="box">
+    <h5>${headline}</h5>
+    <ul class="canban list-group">
+    </ul>
+    <button type="button" class="mybtn list-group-item">Add new card</button>
+</div>`
+    }
+    $('.list-field').html(str);
+    console.log(cols);
+}
+
+// render task !!!!
+
+// Drag N Drop
 
 $( function() {
   $( ".canban" ).sortable({
@@ -16,6 +43,8 @@ $(document).on('mouseup', '.canban li', function() {
   $(this).removeClass("rotate");
   });
 
+// Add Task in Board
+
 $(document).on('click', '.mybtn', function() {
   let str = '';
   arrayTask.forEach((item) => {
@@ -23,11 +52,10 @@ $(document).on('click', '.mybtn', function() {
   });
   $(this.previousElementSibling).html(str+`<li class="list-group-item"><input class="headline"></input></li>`);
   $('.headline').focus();
-  // console.log(arrayTask);
 });
 
 $(document).on('blur', '.headline', function back() {
-  $(this).replaceWith(`<span class="content">${this.value}</span>`);
+  $(this).replaceWith(`<span>${this.value}</span>`);
   const newTask = {
     headline: this.value,
     description: '',
@@ -45,7 +73,26 @@ $(document).on('keydown', '.headline', function enter(event) {
   }
 });
 
-$(document).on('click', '#new-list', function() {
+// Add Board
 
+$(document).on('click', '#new-list', function() {
+//   $('.list-field').html(`<div class="box">
+//   <input class=""headline></input>
+// </div>`)
+// $('.headline').focus();
+
+$('.list-field').html(`<div class="box">
+<input class="headline-col" >
+<ul class="canban list-group">
+</ul>
+<button type="button" class="mybtn list-group-item">Add new card</button>
+</div>`)
 });
+
+$(document).on('blur', '.headline-col', function back() {
+  $(this).replaceWith(`<h5>${this.value}</h5>`);
+  cols.set(this.value, []);
+  render()
+});
+
 });
