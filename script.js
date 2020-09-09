609e94347843e57dd5b81a31bbb47209f9592838
Alex <flalex855@gmail.com>
2020-09-09 18:09:55 +0300

    fix transform bag animation, add col navigation, del all tasks, del column, etc

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 $(document).ready(() => {
 
-let arrayTask = [];
-let cols = new Map ();
+  let arrayTask = [];
+  let cols = new Map();
 
-// Drag N Drop
-const drag = function() {
-  $( function() {
-    $( '.canban ' ).sortable({
-      connectWith: ".canban"
-    }).disableSelection();
-  } );
+  // Drag N Drop
+  const drag = function () {
+    $(function () {
+      $('.canban ').sortable({
+        connectWith: ".canban"
+      }).disableSelection();
+    });
   }
 
-// render cols and task
+  // render cols and task
 
-const render = function() {
-  let strcol = ``;
-  for(let headline of cols.keys()){
-    let str = '';
-    cols.get(headline).forEach((item) => {
-    str += `<li id=${item.id} class="list-group-item">${item.headline}</li>`;
-  })
-    strcol+= `<div class="box">
-    <h5>${headline}</h5>
+  const render = function () {
+    let strcol = ``;
+    for (let headline of cols.keys()) {
+      let str = '';
+      cols.get(headline).forEach((item) => {
+        str += `<li id=${item.id} class="list-group-item">${item.headline}</li>`;
+      })
+      strcol += `<div class="box">
+    <div class="card-opt">
+                    <h5>${headline}</h5>
+                    <button type="button" class="btn btn-info dropdown-toggle dropdown-toggle-split"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="sr-only">Toggle Dropdown</span>
+                    </button>
+                    <div class="dropdown-menu">
+                        <a id="${headline}" class="dropdown-item del-cards" href="#">Delete all cards</a>
+                        <a id="${headline}" class="dropdown-item del-column" href="#">Delete column</a>
+                        <div class="dropdown-divider"></div>
+                        <a id="${headline}" class="dropdown-item copy-cards" href="#">Copy all cards</a>
+                    </div>
+                </div>
     <ul id="${headline}" class="canban list-group">
     ${str}
     </ul>
@@ -31,115 +43,138 @@ const render = function() {
     }
     $('.list-field').html(strcol);
     drag()
-}
+  }
 
-// Add new board
+  // Add new board
 
-const addBoard = function(key, headline) {
-  key = key.replace(/ +/g, ' ').trim();
-  key = key.replace(/&/g, '&amp');
-  key = key.replace(/</g, '&lt');
-  key = key.replace(/>/g, '&gt');
-  key = key.replace(/"/g, '&quot');
-  key = key.replace(/`/g, '&#x60');
-  cols.set(key, []);
-  render()
-}
+  const addBoard = function (key, headline) {
+    key = key.replace(/ +/g, ' ').trim();
+    key = key.replace(/&/g, '&amp');
+    key = key.replace(/</g, '&lt');
+    key = key.replace(/>/g, '&gt');
+    key = key.replace(/"/g, '&quot');
+    key = key.replace(/`/g, '&#x60');
+    cols.set(key, []);
+    render()
+  }
 
-addBoard('My First Board');
+  addBoard('My First Board');
 
+ // Transform task on click
 
-
-$(document).on('mousedown', '.canban li', function() {
-$(this).addClass("rotate");
-});
-
-$(document).on('mouseup', '.canban li', function() {
-  $(this).removeClass("rotate");
+  $(document).on('mousedown', '.canban li', function () {
+    $(this).addClass("rotate");
+    console.log($(this.parentElement));
   });
 
-// Add Task
-
-const addTask = function(key, headline) {
-  key = key.replace(/ +/g, ' ').trim();
-  key = key.replace(/&/g, '&amp');
-  key = key.replace(/</g, '&lt');
-  key = key.replace(/>/g, '&gt');
-  key = key.replace(/"/g, '&quot');
-  key = key.replace(/`/g, '&#x60');
-
-  const newTask = {
-    headline: key,
-    description: '',
-    isChecked: false,
-    id: Date.now(),
-  };
-  cols.set(headline,[...cols.get(headline), newTask])
-}
-
-// click on Add Task
-let fuck = '';
-
-$(document).on('click', '.mybtn', function() {
-  let str = '';
-  arrayTask.forEach((item) => {
-    str += `<li id=${item.id} class="list-group-item">${item.headline}</li>`;
+  $(document).on('mouseup', function () {
+    $('.canban li').removeClass("rotate");
+    console.log($(this.parentElement));
   });
-  $(this.previousElementSibling).append(str+`<li class="list-group-item"><input class="headline"></input></li>`);
-  $('.headline').focus();
-  fuck = $(this.previousElementSibling).attr('id');
-});
 
-$(document).on('blur', '.headline', function back() {
-  if (this.value === '') {
-    $('.headline').remove();
-    render();
-  } else {
-    this.value = this.value.replace(/ +/g, ' ').trim();
-    this.value = this.value.replace(/&/g, '&amp');
-    this.value = this.value.replace(/</g, '&lt');
-    this.value = this.value.replace(/>/g, '&gt');
-    this.value = this.value.replace(/"/g, '&quot');
-    this.value = this.value.replace(/`/g, '&#x60');
-    addTask(this.value, fuck);
-    $(this).replaceWith(`<span>${this.value}</span>`);
-    render();
+  // Add Task
+
+  const addTask = function (key, headline) {
+    key = key.replace(/ +/g, ' ').trim();
+    key = key.replace(/&/g, '&amp');
+    key = key.replace(/</g, '&lt');
+    key = key.replace(/>/g, '&gt');
+    key = key.replace(/"/g, '&quot');
+    key = key.replace(/`/g, '&#x60');
+
+    const newTask = {
+      headline: key,
+      description: '',
+      isChecked: false,
+      id: Date.now(),
+    };
+    cols.set(headline, [...cols.get(headline), newTask])
   }
-});
 
-$(document).on('keydown', '.headline', function enter(event) {
-  if (event.which === 13) {
-  $(this).replaceWith(`<span class="content">${this.value}</span>`);
-  }
-});
+  // click on Add Task
+  let fuck = '';
 
-// Add Board
+  $(document).on('click', '.mybtn', function () {
+    let str = '';
+    arrayTask.forEach((item) => {
+      str += `<li id=${item.id} class="list-group-item">${item.headline}</li>`;
+    });
+    $(this.previousElementSibling).append(str + `<li class="list-group-item"><input class="headline"></input></li>`);
+    $('.headline').focus();
+    fuck = $(this.previousElementSibling).attr('id');
+  });
 
-$(document).on('click', '#new-list', function() {
-$(".headline-col").css("visibility", "visible");
-$('.headline-col').focus();
-});
-
-$(document).on('blur', '.headline-col', function back() {
-  if (this.value === '') {
-    $(".headline-col").css("visibility", "hidden");
-  } else {
-    $(".headline-col").css("visibility", "hidden");
-    addBoard(this.value);
-    this.value = '';
-  }
-});
-
-$(document).on('keydown', '.headline-col', function enter(event) {
-  if (event.which === 13) {
+  $(document).on('blur', '.headline', function back() {
     if (this.value === '') {
-      $('.headline-col').focus();
-  } else {
-    $(".headline-col").css("visibility", "hidden");
-    addBoard(this.value);
-    this.value = '';
-  }
-  }
-});
+      $('.headline').remove();
+      render();
+    } else {
+      this.value = this.value.replace(/ +/g, ' ').trim();
+      this.value = this.value.replace(/&/g, '&amp');
+      this.value = this.value.replace(/</g, '&lt');
+      this.value = this.value.replace(/>/g, '&gt');
+      this.value = this.value.replace(/"/g, '&quot');
+      this.value = this.value.replace(/`/g, '&#x60');
+      addTask(this.value, fuck);
+      $(this).replaceWith(`<span>${this.value}</span>`);
+      render();
+    }
+  });
+
+  $(document).on('keydown', '.headline', function enter(event) {
+    if (event.which === 13) {
+      $(this).replaceWith(`<span class="content">${this.value}</span>`);
+    }
+  });
+
+  // Add Board
+
+  $(document).on('click', '#new-list', function () {
+    $(".headline-col").css("visibility", "visible");
+    $('.headline-col').focus();
+  });
+
+  $(document).on('blur', '.headline-col', function back() {
+    if (this.value === '') {
+      $(".headline-col").css("visibility", "hidden");
+    } else {
+      $(".headline-col").css("visibility", "hidden");
+      addBoard(this.value);
+      this.value = '';
+    }
+  });
+
+  $(document).on('keydown', '.headline-col', function enter(event) {
+    if (event.which === 13) {
+      if (this.value === '') {
+        $('.headline-col').focus();
+      } else {
+        $(".headline-col").css("visibility", "hidden");
+        addBoard(this.value);
+        this.value = '';
+      }
+    }
+  });
+
+  // Delete All Cards
+
+  $(document).on('click', '.del-cards', function () {
+    cols.set($(this).attr('id'), []);
+    render()
+  });
+
+  // Delete Column
+
+  $(document).on('click', '.del-column', function () {
+    cols.delete($(this).attr('id'));
+    render()
+  });
+
+  // Copy All Cards
+
+  $(document).on('click', '.copy-cards', function () {
+    
+  });
+
 
 });
